89e1633203eded533fba94ac7b7e8fbdc1543379
Nelson Jr <juniordell123@gmail.com>
2020-07-24 19:58:07 -0300

    Starting the project

---
+++ src/components/Dropdown/Options.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export function DropdownOption({ name, content: Content }) {
+  return (
+      <button className='dropdown-option'>{name}</button>
+  );
+}
